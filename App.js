1593dde5e5e310b5153ed6d2d676335fe0d10a66
Aiin <aiin_bai@Aiins-MacBook-Pro.local>
2024-04-09 18:07:04 -0700

    complete part3

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://Cluster14526:200096@cluster11643.wdcet3d.mongodb.net/kanbas?retryWrites=true&w=majority&appName=Cluster11643");
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./kanbas/courses/routes.js";
 import ModuleRoutes from "./kanbas/modules/routes.js";
 import AssignmentRoutes from "./kanbas/assignments/routes.js";
+import UserRoutes from "./Users/routes.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,5 +16,7 @@ Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
+
 AssignmentRoutes(app);
 app.listen(process.env.PORT || 4000);
