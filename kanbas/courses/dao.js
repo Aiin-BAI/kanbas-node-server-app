fdcb1dd581a2cb6e3e3c84bfa0aa78e5f0eba211
Aiin-BAI <74397852+Aiin-BAI@users.noreply.github.com>
2024-04-11 23:09:24 -0700

    Revert "A6"

--- kanbas/courses/dao.js
+++
@@ -1,8 +0,0 @@
-import courseModel from "./model.js";
-export const findAllCourses = () => courseModel.find();
-export const findCourseById = (id) => courseModel.findOne({id: id});
-//export const findCourseById = (id) => courseModel.findById(id );
-export const createCourse = (course) => courseModel.create(course);
-export const updateCourse = (id, course) =>
-  courseModel.updateOne({ _id: id }, { $set: course });
-export const deleteCourse = (id) => courseModel.deleteOne({ _id: id });
--- kanbas/modules/model.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-import moduleSchema from "./schema.js";
-const moudleModle = mongoose.model("module",moduleSchema);
-export default moudleModle;
