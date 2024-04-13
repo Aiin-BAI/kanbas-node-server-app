594561dab5d75a3ce29a044b974b949d90fd9f93
Aiin <aiin_bai@Aiins-MacBook-Pro.local>
2024-04-12 19:37:59 -0700

    fix mongodb

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-mongoose.connect("mongodb+srv://Cluster14526:200096@cluster11643.wdcet3d.mongodb.net/kanbas?retryWrites=true&w=majority&appName=Cluster11643");
+mongoose.connect("mongodb+srv://Cluster14526:200096@cluster11643.wdcet3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster11643");
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
@@ -10,6 +10,7 @@ import AssignmentRoutes from "./kanbas/assignments/routes.js";
 import UserRoutes from "./Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
+console.log("Session Secret:", process.env.SESSION_SECRET);
 
 const app = express();
 app.use(
